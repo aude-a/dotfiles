d08cc6c4e4d0cec14251c54dde9837ae0e822465
François-Xavier Colas <fx@colas.io>
2018-08-21 14:59:32 +0200

    install.sh : Fix directory name for symbolic links

--- install.sh
+++ install.sh
@@ -2,33 +2,35 @@
 # Génération d'un espace de travail
 # François-Xavier Colas - 2018
 
+REP="$( cd "$(dirname "$0")" ; pwd -P )"
+
 apt install -y build-essential terminator curl git vim zsh tmux htop fonts-powerline
 
 # Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 mv ~/.zshrc ~/.zshrc_$(date -I).bak
-ln -s ./.zshrc ~/.zshrc
+ln -s $REP/.zshrc ~/.zshrc
 
 # Vim
 mkdir -p ~/.vim/.undo
 mkdir -p ~/.vim/.swp
 mkdir -p ~/.vim/.backup
 mv ~/.vimrc ~/.vimrc_$(date -I).bak
-ln -s ./.vimrc ~/.vimrc
+ln -s $REP/.vimrc ~/.vimrc
 
 # Tmux
 git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
 mv ~/.tmux.conf ~/.tmux.conf_$(date -I).bak
-ln -s ./.tmux.conf ~/.tmux.conf
+ln -s $REP/.tmux.conf ~/.tmux.conf
 
 # Terminator
 mv ~/.config/terminator/config ~/.config/terminator/config_$(date -I).bak
-ln -s ./terminator.confir ~/.config/terminator/config
+ln -s $REP/terminator.config ~/.config/terminator/config
 
 # Monaco font
 mkdir -p /usr/share/fonts/truetype/monaco/
-cp ./monaco.ttf /usr/share/fonts/truetype/monaco/
+cp $REP/monaco.ttf /usr/share/fonts/truetype/monaco/
 fc-cache -f
 
 # Powershell
